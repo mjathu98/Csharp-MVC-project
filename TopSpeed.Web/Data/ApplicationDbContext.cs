cd6ca529703776feb00c01aea06bf38c0d2d673c
mjathu24 <jathuwork@gmail.com>
2024-12-01 22:10:57 +0530

    Add project files.

---
+++ TopSpeed.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TopSpeed.Web.Models;
+
+namespace TopSpeed.Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Brand> Brands { get; set; }
+    }
+}
---
+++ TopSpeed.Web/Models/Brand.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TopSpeed.Web.Models
+{
+    public class Brand
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public string Name { get; set; }=string.Empty;
+       [Display(Name="Established Year")]
+        public int EstablishedYear { get; set; }
+        [Display(Name = "Brand Logo")]
+        public string BrandLogo { get; set; } = string.Empty;
+    }
+}
